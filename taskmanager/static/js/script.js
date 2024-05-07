5862c4c69bbbf55706052cf879e7328af320a9f2
Saranyajayakumaran jayakumaran <jsaran.bj@gmail.com>
2024-05-07 08:58:33 +0000

    Add add task function and wire up with routes.py

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format:"dd mmm,yyyy",
+        i18n : {done:"Select"}
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
+
